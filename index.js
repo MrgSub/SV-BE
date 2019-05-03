ad2f88770503f93fa5a7dc2a41a29b88d938cabb
A <x_1337@outlook.com>
2019-05-03 16:09:05 -0700

    Update getToken

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get('/AuthUrl', (req, res) => {
 });
 
 app.get('/AuthUrl/getToken', (req, res) => {
-	res.send({ req });
+	res.send(req);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
